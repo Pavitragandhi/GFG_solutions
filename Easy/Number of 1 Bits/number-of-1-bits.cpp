5b14a93b657c52506cbe3d88c2446135e31b0bb0
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-24 18:09:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of 1 Bits/number-of-1-bits.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        bitset<32> binary_representation(N);
+        int result = binary_representation.count();
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
