e89dda58f7c62cbbb7a74c5a4dd8291f54c0056d
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-02-17 22:05:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted arrays/intersection-of-two-sorted-arrays.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// C++ program to find union of 
+// two sorted arrays 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to return a list containing the intersection of two arrays.
+    vector<int> printIntersection(int arr1[], int arr2[], int N, int M) 
+    { 
+        vector<int> result;
+        set<int> store1(arr1 , arr1+N);
+        set<int> store2(arr2 , arr2+M);
+        
+        for(auto num : store1){
+            if(store2.find(num) != store2.end()){
+                result.push_back(num);
+            }
+        }
+        return result;
+    }
+
+};
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() 
+{ 
+    int T;
+    cin >> T;
+ 
+    while(T--){
+        
+        int N, M;
+        cin >> N >> M;
+        int arr1[N];
+        int arr2[M];
+        for(int i = 0;i<N;i++){
+            cin >> arr1[i];
+        }
+        
+        for(int i = 0;i<M;i++){
+            cin >> arr2[i];
+        }
+        Solution ob;
+        // Function calling 
+        vector<int> v;
+        v = ob.printIntersection(arr1, arr2, N, M); 
+        
+        for(int i=0;i<v.size();i++)
+            cout<<v[i]<<" ";
+
+        cout << endl;
+    }
+    
+  return 0; 
+} 
+
+// } Driver Code Ends
