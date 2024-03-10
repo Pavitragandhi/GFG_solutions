6488e3814d308d41fe94903758e33e3b2f2d34d7
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-03-10 20:58:22 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Print first letter of every word in the string/print-first-letter-of-every-word-in-the-string.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	string firstAlphabet(string S)
+	{
+	    istringstream s(S);
+	    string read;
+	    string result = "";
+	    
+	    while(s >> read){
+	        result += read[0];
+	    }
+	    return result;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   	
+        Solution ob;
+   		cout << ob.firstAlphabet(s) << "\n";
+   	}
+
+    return 0;
+} 
+
+// } Driver Code Ends
